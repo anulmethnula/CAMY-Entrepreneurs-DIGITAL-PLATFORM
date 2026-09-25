473f7f0bbe1a3404df98ecb4899821e46b36db36
Anul Methnula <299733619+anulmethnula@users.noreply.github.com>
2026-09-25 10:39:20 +0530

    Detach active app from the legacy customer marketplace

--- src/Marketplace.jsx
+++ src/Marketplace.jsx
@@ -1,6 +1,4 @@
-export { AdminSupplyPage, PublicMarketplace } from './MarketplaceLegacy'
 export {
   DropshipHome as ShopHome,
-  DropshipInventoryPage as MyInventoryPage,
   DropshipOrderPage as StockSupplyPage,
 } from './DropshipMarketplace'
